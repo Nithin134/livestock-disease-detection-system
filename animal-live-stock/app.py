bd47dc935217c086c902b7778897157dd1d76710
Nithin134 <133798305+Nithin134@users.noreply.github.com>
2026-03-14 00:02:18 +0530

    Move port initialization outside of main check

--- animal-live-stock/app.py
+++ animal-live-stock/app.py
@@ -24,8 +24,9 @@ def home():
     return render_template("index.html")
 
 
-import os
+
+
+port = int(os.environ.get("PORT", 8080))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
